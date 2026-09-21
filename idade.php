7d233c8c8bcd48b944bce7525c086e3a71b5f37b
oseiasrebelo <oseias.rebelo07@gmail.com>
2026-09-21 15:05:00 -0300

    atualização index

---
+++ idade.php
@@ -0,0 +1,55 @@
+<?php
+    $nome = "";
+    $idade = 0;
+    $resultado = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nome = $_POST["nome"];
+        $idade = $_POST["idade"];
+
+        if ($idade < 18) {
+            $resultado = "menor de idade.";
+        } else {
+            $resultado = "maior de idade.";
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nome e Idade</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <form method="POST">
+
+        <input type="text" id="nome" name="nome" placeholder="Digite seu nome">
+
+        <input type="number" id="idade" name="idade" placeholder="Digite sua idade">
+
+        <button type="submit">Enviar</button>
+
+    </form>
+
+    <?php if ($resultado != "") { ?>
+
+        <div class="container">
+
+            <h1>Nome: <?= $nome ?></h1>
+
+            <h1>Idade: <?= $idade ?></h1>
+
+            <p><?= $nome ?> é <?= $resultado ?></p>
+
+        </div>
+
+    <?php } ?>
+
+</body>
+</html>
+
--- index.php
+++ index.php
@@ -1,20 +1,3 @@
-<?php
-    $nome = "";
-    $idade = 0;
-    $resultado = "";
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $nome = $_POST["nome"];
-        $idade = $_POST["idade"];
-
-        if ($idade < 18) {
-            $resultado = "menor de idade.";
-        } else {
-            $resultado = "maior de idade.";
-        }
-    }
-?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -26,29 +9,8 @@
 
 <body>
 
-    <form method="POST">
+<a href="idade.php">Verificador de idade</a>
 
-        <input type="text" id="nome" name="nome" placeholder="Digite seu nome">
-
-        <input type="number" id="idade" name="idade" placeholder="Digite sua idade">
-
-        <button type="submit">Enviar</button>
-
-    </form>
-
-    <?php if ($resultado != "") { ?>
-
-        <div class="container">
-
-            <h1>Nome: <?= $nome ?></h1>
-
-            <h1>Idade: <?= $idade ?></h1>
-
-            <p><?= $nome ?> é <?= $resultado ?></p>
-
-        </div>
-
-    <?php } ?>
 
 </body>
 </html>
