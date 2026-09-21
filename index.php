baf53e21a85a89dfa3db0c395d17fb5f8a5cc459
oseiasrebelo <oseias.rebelo07@gmail.com>
2026-09-21 14:38:40 -0300

    correção idade

--- index.php
+++ index.php
@@ -1,58 +1,55 @@
 <?php
-    $nome="";
-    $idade=0;
-    $resultado="";
-    
-    if ($_SERVER["REQUEST_METHOD"]=="POST"){
+    $nome = "";
+    $idade = 0;
+    $resultado = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $nome = $_POST["nome"];
         $idade = $_POST["idade"];
 
-        if ($idade <= 18) {
+        if ($idade < 18) {
             $resultado = "menor de idade.";
-        }
-        else {
+        } else {
             $resultado = "maior de idade.";
         }
-
     }
-    ?>
+?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Nome e Idade</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-        
+
     <form method="POST">
-        <input type="text" id="nome" name="nome">
-        <input type="text" id="idade" idade="idade">
 
-        
+        <input type="text" id="nome" name="nome" placeholder="Digite seu nome">
 
-    <?php if($resultado != "") {?>
-        <p> <?=$nome?> é <?=$resultado ?></p>
-        <p> <?=$idade?> é <?=$resultado ?></p>     
+        <input type="number" id="idade" name="idade" placeholder="Digite sua idade">
 
-    <?php } ?>
+        <button type="submit">Enviar</button>
 
     </form>
 
-    
+    <?php if ($resultado != "") { ?>
 
+        <div class="container">
 
+            <h1>Nome: <?= $nome ?></h1>
 
-<div class="container">
+            <h1>Idade: <?= $idade ?></h1>
 
-        <h1> Nome: <?=$nome ?></h1>
-        <h1> Idade: <?=$idade ?></h1>
-      
-    </div>
+            <p><?= $nome ?> é <?= $resultado ?></p>
 
-        
+        </div>
+
+    <?php } ?>
+
 </body>
 </html>
 
