c42b84bbb7a1829104862c44d6d9787b66025c55
oseiasrebelo <oseias.rebelo07@gmail.com>
2026-09-23 13:52:18 -0300

    index css

--- index.php
+++ index.php
@@ -3,16 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nome e Idade</title>
-    <link rel="stylesheet" href="index.css">
+
+    <title>Teste CSS</title>
+
+    <link rel="stylesheet" href="./index.css">
 </head>
 
 <body>
 
-<a href="idade.php">Verificador de idade</a>
-<a href="notas.php">ir para notas</a>
+    <h1>TESTE</h1>
 
+    <a href="idade.php">Verificador de idade</a>
+    <a href="notas.php">Ir para notas</a>
 
 </body>
 </html>
-
