e95c547311f1c06b0f69aa9b01abc3b148dcc4ca
oseiasrebelo <oseias.rebelo07@gmail.com>
2026-09-23 14:10:52 -0300

    style css

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nome e Idade</title>
 
-    <link rel="stylesheet" href="./index.css">
+    <link rel="stylesheet" href="./style.css">
 </head>
 
 <body>
